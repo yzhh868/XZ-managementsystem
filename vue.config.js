74e1b0567df6a79bbbe808553befc0676bd651e0
yzhh868 <yzhh868@163.com>
2021-11-24 12:43:31 +0800

    配置路径别名

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+// 配置文件的路径别名
+
+module.exports={
+    configureWebpack:{
+        resolve:{
+           alias:{
+               'assets':'@/assets',
+               'common':'@/common',
+               'components':'@/components',
+               'network':'@/network',
+               'views':'@/views',
+           }
+        }
+    }
+}
