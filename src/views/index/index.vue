54e401b6f47bb2d28a86cfa09931b85465dbf5b0
yzhh868 <yzhh868@163.com>
2021-11-24 22:22:40 +0800

    菜单页面已经更新

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,19 +1,85 @@
 <template>
-  <div id="index">
-    <base-information/>
-  </div>
+  <a-layout class="layout" style="min-height: 100vh">
+    <a-layout-sider v-model="collapsed" collapsible>
+      <div class="logo" />
+      <a-menu theme="dark" :default-selected-keys="['1']" mode="inline">
+        <a-menu-item key="1">
+          <a-icon type="pie-chart" />
+          <span>Option 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <a-icon type="desktop" />
+          <span>Option 2</span>
+        </a-menu-item>
+        <a-sub-menu key="sub1">
+          <span slot="title"><a-icon type="user" /><span>User</span></span>
+          <a-menu-item key="3">
+            Tom
+          </a-menu-item>
+          <a-menu-item key="4">
+            Bill
+          </a-menu-item>
+          <a-menu-item key="5">
+            Alex
+          </a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <span slot="title"><a-icon type="team" /><span>Team</span></span>
+          <a-menu-item key="6">
+            Team 1
+          </a-menu-item>
+          <a-menu-item key="8">
+            Team 2
+          </a-menu-item>
+        </a-sub-menu>
+        <a-menu-item key="9">
+          <a-icon type="file" />
+          <span>File</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #53e5ec ; padding: 0; min-height: 8vh" >
+        <div class="myfont">
+          兴足进销存管理系统
+        </div>
+      </a-layout-header>
+      <a-layout-content >
+        <div :style="{ padding: '24px', background: '#fff', minHeight: '70vh' }">
+          <router-view></router-view>
+        </div>
+      </a-layout-content>
+      <a-layout-footer style="text-align: center">
+        Ant Design ©2018 Created by Ant UED
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
 </template>
-
 <script>
-import baseInformation from "../../components/content/baseInformation";
 export default {
-  name: "index",
-  components:{
-    baseInformation
-  }
-}
+  name:'index',
+  data() {
+    return {
+      collapsed: false,
+    };
+  },
+};
 </script>
 
-<style scoped>
-
+<style>
+.layout .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
+.myfont{
+  margin-top: 2px;
+  font-size: 48px;
+  font-family: 楷体;
+  text-align: center;
+  letter-spacing: 2px;
+  font-weight: bold;
+  color: #ffffff;
+}
 </style>
+
